4c0a370fa608775df65ec343059b9e86286ae472
All-Chat <deepak@therealleads.com>
2026-06-21 15:45:50 +0530

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from "next-auth"
 
 declare module "next-auth" {
   interface Session {
     user: {
+      whiteLabel: any
       tenantId: string | null | undefined
       id: string
       name?: string | null
