28d4cc675127fe63c46d74ae1cabfe37e23648db
All-Chat <deepak@therealleads.com>
2026-07-12 16:42:24 +0530

    Update queue.ts

--- lib/queue.ts
+++ lib/queue.ts
@@ -5,7 +5,7 @@ export const connection = {
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: parseInt(process.env.REDIS_PORT || '6379'),
   password: process.env.REDIS_PASSWORD || undefined,
-  tls: process.env.REDIS_HOST ? {} : undefined, 
+  tls: process.env.REDIS_HOST && process.env.REDIS_HOST !== '127.0.0.1' ? {} : undefined, 
   maxRetriesPerRequest: null,
   keepAlive: 30000,
   enableReadyCheck: false,
