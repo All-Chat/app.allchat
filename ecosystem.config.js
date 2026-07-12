da3db5024dadc775ef9b255d9fb7d708a820b999
All-Chat <deepak@therealleads.com>
2026-07-12 16:43:32 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3999", 
       instances: 1,
-      exec_mode: "fork", // <--- ADD THIS
+      exec_mode: "fork",
       autorestart: true,
       max_memory_restart: "1G",
       env: {
@@ -17,9 +17,9 @@ module.exports = {
       name: "whatsapp-worker",
       cwd: __dirname,
       script: "npx",
-      args: "tsx worker.ts",
+      args: "tsx workers/mainWorker.ts",
       instances: 1,
-      exec_mode: "fork", // <--- ADD THIS
+      exec_mode: "fork",
       autorestart: true,
       max_memory_restart: "1G",
       env: {
